a4de7417dd0a648ef683ab465d6d23cc5bbc5717
lobracost <pa.koromilas@gmail.com>
2021-09-23 19:03:52 +0300

    feature normalization per modality

--- utils/dataloading.py
+++ utils/dataloading.py
@@ -3,6 +3,7 @@ import sys
 import yaml
 from tqdm import tqdm
 import numpy as np
+from numpy import inf
 import random
 import torch
 from torch.utils.data import Dataset
@@ -28,6 +29,31 @@ class RepresentationDataset(Dataset):
         return self.X[index]
 
 
+def scale(x, maximum=None, minimum=None):
+
+    if maximum is None and minimum is None:
+        maximum = x.max()
+        minimum = x.min()
+        if minimum == -inf:
+            tmp = x
+            tmp[tmp == -inf] = 0
+            minimum = tmp.min()
+            x[x == -inf] = minimum
+        return (x - minimum) / (maximum - minimum), maximum, minimum
+    else:
+        if x.min() == -inf:
+            x[x == -inf] = minimum
+        return (x - minimum) / (maximum - minimum)
+
+
+def scale_by_modality(x_train, x_val, x_test, up, down):
+    x_train[:, :, up:down], maximum, minimum = scale(x_train[:, :, up:down])
+    x_val[:, :, up:down] = scale(x_val[:, :, up:down], maximum, minimum)
+    x_test[:, :, up:down] = scale(x_test[:, :, up:down], maximum, minimum)
+
+    return x_train, x_val, x_test
+
+
 def dataloaders():
     torch.cuda.empty_cache()
     seed = 42
@@ -36,6 +62,10 @@ def dataloaders():
     dataset_name = "cmumosei"
     seq_len = 20
     x_train, y_train, x_val, y_val, x_test, y_test = get_feature_matrix(dataset_name, seq_len)
+
+    x_train, x_val, x_test = scale_by_modality(x_train, x_val, x_test, 0, 74)
+    x_train, x_val, x_test = scale_by_modality(x_train, x_val, x_test, 74, 109)
+    x_train, x_val, x_test = scale_by_modality(x_train, x_val, x_test, 109, 409)
 
     # TODO: scaling
 
