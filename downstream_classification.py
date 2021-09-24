cd65a30a652f6a62572bb35ad4483e6e81c9be9d
lobracost <pa.koromilas@gmail.com>
2021-09-25 02:49:41 +0300

    run both iemocap + mosei classifiction

--- downstream_classification.py
+++ downstream_classification.py
@@ -7,6 +7,7 @@ from imblearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE
 from imblearn.combine import SMOTETomek
+from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -54,7 +55,7 @@ device = torch.device("cuda:0" if use_cuda else "cpu")
 print("Running on: {}".format(device))
 
 model = architectures.ConvAutoEncoder(20, 409).to(device)
-model.load_state_dict(torch.load('output/ConvAE_all.pt'))
+model.load_state_dict(torch.load('output/ConvAE_mosei_iemocap.pt'))
 
 dataset_name = "iemocap"
 train_loader, val_loader, test_loader = dataloading.classification_dataloaders(dataset_name)
@@ -65,26 +66,38 @@ x_train = np.concatenate((x_train, x_val))
 y_train = y_train + y_val
 x_test, y_test = get_representations(model, test_loader, device)
 
-if dataset_name == "iemocap":
-    iemocap_emotions = ["Neutral", "Happy", "Sad", "Angry"]
-    y_train_init = y_train
-    y_test_init = y_test
-    for label in range(4):
-        y_train = one_vs_all_labels(y_train_init, label)
-        y_test = one_vs_all_labels(y_test_init, label)
-        print("-------------------Label {}-------------------".format(iemocap_emotions[label]))
-        print(Counter(y_train))
-        pipe = Pipeline([('scaler', StandardScaler()), ('clf', LogisticRegression())])
+print("---> Classification for IEMOCAP")
 
-        pipe.fit(x_train, y_train)
-        preds = pipe.predict(x_test)
-
-        print(classification_report(y_test, preds))
-else:
+iemocap_emotions = ["Neutral", "Happy", "Sad", "Angry"]
+y_train_init = y_train
+y_test_init = y_test
+for label in range(4):
+    y_train = one_vs_all_labels(y_train_init, label)
+    y_test = one_vs_all_labels(y_test_init, label)
+    print("       -------------------Label {}-------------------".format(iemocap_emotions[label]))
     print(Counter(y_train))
-    pipe = Pipeline([('scaler', StandardScaler()), ('clf', LogisticRegression())])
+    pipe = Pipeline([('scaler', StandardScaler()), ('clf', LogisticRegression(random_state=42))])
 
     pipe.fit(x_train, y_train)
     preds = pipe.predict(x_test)
 
     print(classification_report(y_test, preds))
+
+print("---> Classification for CMU-MOSEI")
+
+dataset_name = "cmumosei"
+train_loader, val_loader, test_loader = dataloading.classification_dataloaders(dataset_name)
+
+x_train, y_train = get_representations(model, train_loader, device)
+x_val, y_val = get_representations(model, val_loader, device)
+x_train = np.concatenate((x_train, x_val))
+y_train = y_train + y_val
+x_test, y_test = get_representations(model, test_loader, device)
+
+print(Counter(y_train))
+pipe = Pipeline([('scaler', StandardScaler()), ('clf', LogisticRegression(random_state=42))])
+
+pipe.fit(x_train, y_train)
+preds = pipe.predict(x_test)
+
+print(classification_report(y_test, preds))
